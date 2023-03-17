7b5bee106d5ef347447f766fc4e7d57ac4d4ecac
baziz_meriem <mrmbaziz@gmail.com>
2023-03-17 18:46:50 +0100

    fix font issue

---
+++ app/src/main/java/com/example/flavorfleet/TextInputLabel.kt
@@ -0,0 +1,36 @@
+package com.example.flavorfleet
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+@SuppressLint("ResourceAsColor")
+class TextInputLabel @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+
+    init {
+        context.theme.obtainStyledAttributes(
+            attrs, R.styleable.TextInputLabel, 0, 0
+        ).apply {
+            try {
+                val labelText = getString(R.styleable.TextInputLabel_labelText)
+                val labelTextColor = getColor(R.styleable.TextInputLabel_labelTextColor,Color.BLACK )
+                val labelTextSize = getDimension(R.styleable.TextInputLabel_labelTextSize, 16f)
+
+                text = labelText
+                setTextColor(labelTextColor)
+                textSize = labelTextSize / resources.displayMetrics.density
+                typeface = Typeface.createFromAsset(
+                    context.assets,
+                    "font/Quicksand-Bold.ttf"
+                )
+            } finally {
+                recycle()
+            }
+        }
+    }
+}
